4e3297fbd8ab25d77f00baeec219d3808718e507
Tatieli Ramos <tatielirb@gmail.com>
2019-07-17 21:53:52 -0300

    Controle de repetição inicial

---
+++ udemy-curso-js/exercicios-js/controle/estruturaIF1.js
@@ -0,0 +1,31 @@
+//video 54
+
+function soBoaNoticia(nota) {
+  if(nota >= 7) {
+    console.log('Aprocado com' + nota)
+  }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+function seForVerdadeEuFalo(valor) {
+  if(valor) {
+    console.log('é verdade...' + valor)
+  }
+}
+
+//falso
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+//verdade
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1,2])
+seForVerdadeEuFalo({})
---
+++ udemy-curso-js/exercicios-js/controle/estruturaIf2.js
@@ -0,0 +1,33 @@
+//video 55
+
+function teste1(num) {
+  if(num > 7)
+    console.log(num)
+    console.log('final')
+}
+
+teste1(6)
+teste1(8)
+
+//não use ; no final da definição do bloco if, não usar com as estruturas de cosntrole
+
+function teste2(num) {
+  if(num > 7); {
+    console.log(num)
+  }
+}
+teste2(6)
+teste2(8)
+
+function teste3(num) {
+  if(num > 7)
+    ;
+  {
+    console.log(num)
+  }
+}
+
+teste3(6)
+teste3(8)
+
+
