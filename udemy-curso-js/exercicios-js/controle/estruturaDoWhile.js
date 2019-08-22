c0e61a26310932bdca5aca7c9d584c1756bb4bde
Tatieli Ramos <tatielirb@gmail.com>
2019-08-22 14:53:03 -0300

    Added files functions js course new.

---
+++ udemy-curso-js/exercicios-js/controle/estruturaDoWhile.js
@@ -0,0 +1,15 @@
+//video 60
+
+function getInteiroAletorioEntre(min, max) {
+  const valor = Math.random() * (max - min)  + min
+  return Math.floor(valor)
+}
+
+let opcao = 0
+
+do {
+  opcao = getInteiroAletorioEntre(-1, 10)
+  console.log(`opção escolhida foi`, opcao)
+}while (opcao != -1) 
+
+console.log('até a proxima')
---
+++ udemy-curso-js/exercicios-js/controle/estruturaWhile.js
@@ -0,0 +1,14 @@
+//video 59
+function getInteiroAletorioEntre(min, max) {
+  const valor = Math.random() * (max - min)  + min
+  return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+  opcao = getInteiroAletorioEntre(-1, 10)
+  console.log(`opção escolhida foi ${opcao}`)
+}
+
+console.log('até a proxima')
