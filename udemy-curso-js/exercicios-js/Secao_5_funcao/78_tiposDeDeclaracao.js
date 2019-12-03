bf71505ba613ee2574005c25473fe23b90232387
Tatieli Ramos <tatielirb@gmail.com>
2019-12-02 22:39:19 -0300

    Functions

---
+++ udemy-curso-js/exercicios-js/Secao_5_funcao/78_tiposDeDeclaracao.js
@@ -0,0 +1,21 @@
+//video 78
+// tipos de decaração
+
+
+console.log(soma(3, 4))
+// function declaration
+function soma(x, y) {
+  return x + y
+}
+
+// function expression
+const sub = function (x, y)  {
+  return x - y
+}
+console.log(soma(3, 4))
+
+// named fucntion expression
+ const mult = function mult(x, y) {
+   return x * y
+ }
+ console.log(soma(3, 4))
